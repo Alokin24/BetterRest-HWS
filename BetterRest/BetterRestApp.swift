bd6f519c0ab96446b71c5a1e046166cf556b265b
Nikola Anastasovski <nikola.anastasovski@netcetera.com>
2021-03-14 14:32:14 +0100

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Nikola Anastasovski on 14.3.21.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
